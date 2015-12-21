e8d5d88d54e49d9312759f8b21ab481bbaea02c6
jinfei <ascode@icloud.com>
2015-12-21 17:57:15 +0800

    添加了播放、暂停、停止播放音乐的功能

--- playsound/ViewController.swift
+++ playsound/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var aPlayer : AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        aPlayer = try! AVAudioPlayer(contentsOfURL: NSBundle.mainBundle().URLForResource("少女时代-TaeTiSeo - Twinkle", withExtension: "mp3")!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func playPressed(sender: AnyObject) {
+        aPlayer.play()
+    }
 
 
+    @IBAction func stopPressed(sender: AnyObject) {
+        aPlayer.stop()
+    }
+    
+    
+    @IBAction func pausePressed(sender: AnyObject) {
+        aPlayer.pause()
+    }
+    
 }
 
